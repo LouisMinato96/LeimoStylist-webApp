445330317fb6bff5c5cbd14baa9ea437db4db0e7
Shubham Mandal <shubham.m@bigphi.com>
2020-10-05 20:40:35 +0530

    Rwact Shell - With Routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import logo from './logo.svg';
 import classes from './App.module.scss';
 import Layout from './HOC/Layout/Layout';
+import Home from './containers/Home/home';
 
 function App() {
   return (
-    <div className={classes.LayOutContainer}>
-      <Layout>
-        <img src={logo} alt="" />
-      </Layout>
-    </div>
+    <BrowserRouter>
+      <div className={classes.LayOutContainer}>
+        <Layout>
+        <Route exact path="/" component={Home} />
+          <img src={logo} alt="" />
+        </Layout>
+      </div>
+    </BrowserRouter>
   );
 }
 
